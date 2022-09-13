d700446184215b2a09ffe0e04e70ac8db16229cd
Patricia <patyamiga16@hotmail.com>
2022-09-13 10:16:14 -0300

    aula16

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -3,6 +3,8 @@ import { AnonymousSubject } from 'rxjs/internal/Subject';
 
 import { Animal } from 'src/app/Animal';
 
+import { ListService } from 'src/app/services/list.service';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
@@ -24,7 +26,7 @@ export class ListRenderComponent implements OnInit {
 
   animalDetails = ''
 
-  constructor() { }
+  constructor( private listService: ListService) { }
 
   ngOnInit(): void {
   }
@@ -33,4 +35,11 @@ export class ListRenderComponent implements OnInit {
     this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
 
   }
+
+  removeAnimal(animal : Animal) {
+    console.log('Removendo animal...');
+    this.animals = this.listService.remove(this.animals, animal);
+
+  }
+
 }
